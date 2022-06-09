b9d6bacb7b657053a3f47f2b65624bada5667edd
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-06-09 14:45:37 -0300

    Finished Sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,11 +10,18 @@ export function Sidebar() {
         src="https://images.pexels.com/photos/5327045/pexels-photo-5327045.jpeg" 
       />
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://github.com/vargastm.png" 
+        />
         <strong>Tiago Martins Vargas</strong>
         <span>Web Developer</span>
 
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20}/>
+            Editar seu perfil
+          </a>
         </footer>
       </div>
     </aside>
